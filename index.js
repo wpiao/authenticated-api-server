66a02b386735b1c828a40504ee099da804190e34
Wenhao Piao <wenhaopiao520@gmail.com>
2020-05-03 17:08:58 -0700

    Set up server and mongoDB

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const { start } = require('./lib/server.js');
+
+dotenv.config();
+
+const options = {
+  useNewUrlParser: true
+}
+
+mongoose.connect(process.env.MONGODB_URL, options);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => console.log('moongodb is connected successfully!'));
+
+start();
